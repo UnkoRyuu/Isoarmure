bd924fac559065510386d1035a363eab49b13ac2
UnkoRyuu <quentin.bernard24@orange.fr>
2020-02-11 15:00:53 -0500

    map réalisé
    deplacement de fichier pour organisation + création de la scene de jeu

---
+++ Isoarmure/Assets/Script/gameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public void onTrackingFound() {
+        Debug.Log("found");
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+    public void onTrackingLost()
+    {
+        Debug.Log("lost");
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+}
