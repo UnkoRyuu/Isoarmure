231f359920f5402922e17cddad35937f8f829fd0
UnkoRyuu <quentin.bernard24@orange.fr>
2020-02-11 21:21:42 -0500

    interface gameplay sans action fait

--- Isoarmure/Assets/Script/BarreDeViePlayer.cs
+++ Isoarmure/Assets/Script/BarreDeViePlayer.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BarreDeViePlayer : MonoBehaviour
 {
-    VieDuJoueur vieDuJoueur;
+    //VieDuJoueur vieDuJoueur;
     // Start is called before the first frame update
     void Start()
     {
