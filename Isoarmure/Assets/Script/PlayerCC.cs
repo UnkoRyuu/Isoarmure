6f5d8606e46cf30daab5e2e2001245fd144cb0a7
A-Evann <ambroisinevann@gmail.com>
2020-02-13 09:05:25 -0500

    pour pull

---
+++ Isoarmure/Assets/Script/PlayerCC.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCC : MonoBehaviour
+{
+
+    public Animator anim;
+    public float speed = 8f;
+    public bool isAttacking = false;
+    private Vector3 moveDirection = Vector3.zero;
+    CharacterController Cc;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Animation Déplacement
+        if (Cc.isGrounded)
+        {
+            moveDirection = new Vector3(0, 0, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+        }
+        anim.SetFloat("vertical", Input.GetAxis("Vertical"));
+        anim.SetFloat("horizontal", Input.GetAxis("Horizontal"));
+
+        //Animation Attack
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+        anim.SetBool("isAttacking", false);
+
+
+        //Mouvement
+        //Rotation du personnage
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * Time.deltaTime * speed * 50);
+
+        //Application du mouvement
+        Cc.Move(moveDirection * Time.deltaTime);
+    }
+}
