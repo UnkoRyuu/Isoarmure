dc7eb89146c5194b4cffdcbda141ee68e0411052
Lucas <lucas.portal@orange.fr>
2020-02-13 08:41:29 -0500

    Modification qui marche toujours pas

--- Isoarmure/Assets/Script/GameManager.cs
+++ Isoarmure/Assets/Script/GameManager.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void PlayGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        SceneManager.LoadScene("scene de jeu", LoadSceneMode.Additive);
     }
 }
