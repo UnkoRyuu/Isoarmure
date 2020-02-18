b5c8428d9588500f731470907ce10441c36c19f8
UnkoRyuu <quentin.bernard24@orange.fr>
2020-02-18 17:24:02 -0500

    ajout maptest

---
+++ Isoarmure/Assets/Script/deplacement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deplacement : MonoBehaviour
+{
+    protected bl_Joystick joystick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<bl_Joystick>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigibody = GetComponent<Rigidbody>();
+        rigibody.velocity = new Vector3(joystick.Horizontal *0.1f , rigibody.velocity.y, joystick.Vertical * 0.1f  );
+    }
+}
