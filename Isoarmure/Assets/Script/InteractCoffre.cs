879f61573b429abde0aa59f14d4161ee426bf866
Lucas <lucas.portal@orange.fr>
2020-03-01 16:21:42 -0500

    Avancement coffre et menu option
    PROBLEME
    La map test_Coffre fait tout plein d'erreur (+999) et disparait ne me laissant que la colisionBox

--- Isoarmure/Assets/Script/InteractCoffre.cs
+++ Isoarmure/Assets/Script/InteractCoffre.cs
@@ -2,7 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractCoffre : MonoBehaviour
+public class InteractCoffre : InteractableItemBase
 {
-    public 
+
+    private bool chestIsOpen = false;
+
+    public override void OnInteract()
+    {
+        InteractText = "Press F to";
+
+        chestIsOpen = !chestIsOpen;
+        InteractText += chestIsOpen ? "close" : "open";
+        
+        GetComponent<Animator>().SetBool("open", chestIsOpen);
+    }
+
 }
