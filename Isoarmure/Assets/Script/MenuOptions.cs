e91be6bed8ee7046d8c642779cbcb5e552fb1295
Lucas <lucas.portal@orange.fr>
2020-03-09 13:31:37 -0400

    modification

--- Isoarmure/Assets/Script/MenuOptions.cs
+++ Isoarmure/Assets/Script/MenuOptions.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MenuOptions : MonoBehaviour
 {
+
+    public AudioMixer audioMixer;
 
     public GameObject panel_Option;
     
@@ -15,4 +18,9 @@ public class MenuOptions : MonoBehaviour
             panel_Option.setActive(!isActive);
         }
     }**/
+
+    public void setVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
 }
