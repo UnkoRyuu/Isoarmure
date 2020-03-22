268d343cf606830d9b63b18fe9b6825f35b10cc7
UnkoRyuu <quentin.bernard24@orange.fr>
2020-03-21 20:00:23 -0400

    logo victoire valid sans son

--- Isoarmure/Assets/Script/Coffre/InteractCoffre.cs
+++ Isoarmure/Assets/Script/Coffre/InteractCoffre.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class InteractCoffre : InteractableItemBase
 {
     private bool chestIsOpen = false;
-
-    
-
+    public GameObject panel_victoire;
+    public Animator anim;
 
     public override void OnInteract()
     {
         chestIsOpen = !chestIsOpen;
         GetComponentInChildren<Animator>().SetBool("open", chestIsOpen);
-    }
 
+        panel_victoire.SetActive(true);
+        
+        
+        
+    }
+    
 }
